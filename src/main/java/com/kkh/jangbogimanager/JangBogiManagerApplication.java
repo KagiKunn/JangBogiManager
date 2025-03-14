12d8aa3602eeb87be480d480f2b7383e8d665501
kkh <key9910@gmail.com>
2025-03-14 12:01:25 +0900

    init

---
+++ src/main/java/com/kkh/jangbogimanager/JangBogiManagerApplication.java
@@ -0,0 +1,13 @@
+package com.kkh.jangbogimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangBogiManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangBogiManagerApplication.class, args);
+	}
+
+}
