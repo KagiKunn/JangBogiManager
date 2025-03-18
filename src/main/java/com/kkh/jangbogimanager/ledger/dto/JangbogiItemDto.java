6c858145248aa635c5039076d07c2694e7518796
kkh <key9910@gmail.com>
2025-03-18 09:35:49 +0900

    tmp

--- src/main/java/com/kkh/jangbogimanager/ledger/dto/JangbogiItemDto.java
+++ src/main/java/com/kkh/jangbogimanager/ledger/dto/JangbogiItemDto.java
@@ -2,8 +2,6 @@ package com.kkh.jangbogimanager.ledger.dto;
 
 import com.kkh.jangbogimanager.ledger.Ledger;
 import com.kkh.jangbogimanager.member.entity.Member;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
